dcacbd7d218d9e5fe43fed5f0a75ccbd42f374cc
dany gbelidji <dany.gbelidji@live.fr>
2021-03-26 15:27:48 +0100

    📝 ADD : 01-hello_world.js > ajout commentaire

--- 01-hello_world.js
+++ 01-hello_world.js
@@ -4,6 +4,8 @@ let port = process.argv[2];
 // let port = 5000;
 
 const app = express();
+
+// on retourne hello world, quand une requete get est émise sur l'URL /home :
 app.get('/home', (request, response) => {
     // response.send();
     response.end('Bonjour, monde !')
